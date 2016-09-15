8ceac7183452887f942ef0c607d2623b4d69522e
urshofer <u.hofer@tr51.org>
2016-09-15 16:53:55 +0200

    Added a second route to show Linking

--- src/routes.php
+++ src/routes.php
@@ -19,3 +19,14 @@ $app->get('/', function ($request, $response, $args) {
   }
 }); 
 
+$app->get('/Book/{id:[0-9]*}', function ($request, $response, $args) {
+  $result = $this->api->get("books/".$args['id']);
+  if($result->info->http_code == 200) {
+       $args = $result->decode_response();
+       $this->view->render($response, 'book.jade', $args);
+  }
+  else {
+    throw new NotFoundException($request, $response);
+  }
+}); 
+
