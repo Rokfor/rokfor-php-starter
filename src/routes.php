0f257aa16507d40b95e80a0d2dc4d9db3c748286
urshofer <u.hofer@tr51.org>
2016-09-15 17:40:53 +0200

    Added additional $args for template injection

--- src/routes.php
+++ src/routes.php
@@ -12,6 +12,12 @@ $app->get('/', function ($request, $response, $args) {
   $result = $this->api->get("books");
   if($result->info->http_code == 200) {
        $args = $result->decode_response();
+       // Any kind of data can be pushed to the $args array
+       $args['__rawData']  = print_r($args, true);
+       $args['__pageTitle'] = "Book Overview";
+       // $args will be expanded and exposed in the jade template.
+       // $args['__pageTitle'] will be available as __pageTitle or
+       // $__pageTitle if used in a more complex string.
        $this->view->render($response, 'index.jade', $args);
   }
   else {
@@ -23,6 +29,8 @@ $app->get('/Book/{id:[0-9]*}', function ($request, $response, $args) {
   $result = $this->api->get("books/".$args['id']);
   if($result->info->http_code == 200) {
        $args = $result->decode_response();
+       $args['__rawData']   = print_r($args, true);
+       $args['__pageTitle'] = "Book Detail";
        $this->view->render($response, 'book.jade', $args);
   }
   else {
